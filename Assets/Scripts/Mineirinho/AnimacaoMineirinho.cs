b7f4f5995e4393fa148f8047ca2fb5ca8314058c
Leonardo Marques <leomarques1905@gmail.com>
2022-08-04 17:35:09 -0300

    movimentacao do mineirinho com reboladinha

---
+++ Assets/Scripts/Mineirinho/AnimacaoMineirinho.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacaoMineirinho : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private Rigidbody rb;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.velocity.magnitude>0)
+        {
+            anim.SetBool("Correndo",true);
+        }
+        else
+        {
+            anim.SetBool("Correndo",false);
+        }
+    }
+}
